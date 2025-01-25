59e444bce3131c3efaae429adccf3d35608a2d13
= <=>
2025-01-25 16:38:49 +0200

    build: sql Copy Edit Add PDF text extraction and language definition functions

---
+++ bot/utils/utilties.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" """
+from ..config import conversation_states
+import pypdf
+
+
+def define_language(state, user_language) -> str:
+    """ """
+    return (
+        conversation_states[state]['en']
+        if user_language == 'en'
+        else conversation_states[state]['ar']
+    )
+
+
+def extract_text(path):
+    text = ""
+    with open(path, "rb") as f:
+        reader = pypdf.PdfReader(f)
+
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
