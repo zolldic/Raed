0572fcc21312abee851546ef893887a250ea74b8
= <=>
2025-01-27 22:24:56 +0200

    refactor: add logger and move bot conversation to the function

--- bot/states/start_conversation.py
+++ bot/states/start_conversation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-"""Module for starting the conversation with the user
-    and determining if they are an activist or an organization.
+"""This module contains the start_conversation state for the Raed bot.
+
+Functions:
+    start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        Initiates a conversation with the user, presenting a welcome message and language selection options.
 """
 from telegram import (
     Update,
@@ -9,25 +12,49 @@ from telegram import (
 )
 from telegram.ext import ContextTypes
 from .. import SET_LANGUAGE
-from ..utils.utilties import define_language
+from ..utils.utilties import define_lang
+
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Starts the conversation and determines the user's type (activist or organization).
-
-    This function initiates the conversation with the user, determines if the user
-    identifies as an activist or an organization, and saves user information such as
-    username, language code, and chat ID to the database.
-
+    """ Initiates a conversation with the user and prompts them to select their preferred language.
+    This function sends a welcome message to the user, providing information about the bot's purpose and available commands.
+    It then asks the user to confirm their preferred language (English or Arabic) and sets up a reply keyboard for language selection.
     Args:
-        update (telegram.Update): The update object that contains information about
-            the incoming message and its metadata.
-        context (telegram.ext.ContextTypes.DEFAULT_TYPE): The context object that
-            contains user-specific data for the conversation.
-
+        update (Update): An object that contains all the information and data that the update event carries.
+        context (ContextTypes.DEFAULT_TYPE): An object that contains the context for the conversation, including user data.
     Returns:
-        int: The next conversation state.
+        int: The next state in the conversation, which is SET_LANGUAGE.
     """
+
+    conversation: dict[str] = {
+        'en': ''.join((
+            "<b>Welcome to Raed, the Activist Support Bot!</b>\n",
+            "Raed is designed to assist CSOs, activists, and changemakers in crafting impactful concept notes, full proposals, and analyzing social issues using structured tools like the Problem Tree method.",
+            "With the support of AI, we aim to simplify the process of writing and problem analysis, allowing you to focus on driving meaningful change in your community.",
+            "\n<b>Available Commands:</b>",
+            "\n/start - Begin a conversation with Raed or restart it.",
+            "\n/cancel - End the conversation at any time.",
+            "\n/info - Get information about Raed and its features.",
+            "\n\n<b>Please confirm your preferred language:</b>",
+            "\nType 'English' to continue in English or 'Arabic' to continue in Arabic."
+        )),
+
+        'ar': ''.join((
+            "<b>مرحبًا بك في رائد، بوت دعم النشطاء!</b>\n",
+            "رائد مصمم لمساعدة منظمات المجتمع المدني، النشطاء، وصناع التغيير في إعداد مذكرات مفاهيمية مؤثرة، مقترحات كاملة، وتحليل القضايا الاجتماعية باستخدام أدوات منظمة مثل طريقة شجرة المشكلة.",
+            "بدعم من الذكاء الاصطناعي، نسعى لتبسيط عملية الكتابة والتحليل، مما يتيح لك التركيز على تحقيق تغيير حقيقي في مجتمعك.",
+            "\n<b>الأوامر المتاحة:</b>",
+            "\n/start - بدء محادثة مع رائد أو إعادة تشغيلها.",
+            "\n/cancel - إنهاء المحادثة في أي وقت.",
+            "\n/info - الحصول على معلومات حول رائد وميزاته.",
+            "\n\n<b>يرجى تأكيد لغتك المفضلة:</b>",
+            "\nاكتب 'English' للمتابعة باللغة الإنجليزية أو 'Arabic' للمتابعة باللغة العربية."
+        ))
+    }
 
     user: User = update.effective_user
 
@@ -35,9 +62,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data['full_name'] = user.full_name
     context.user_data['username'] = user.username
 
-    html_text: str = define_language('start', user.language_code)
+    text: str = define_lang(conversation, user.language_code)
+
     await update.message.reply_text(
-        html_text,
+        text,
         parse_mode='HTML',
         reply_markup=ReplyKeyboardMarkup(
 
@@ -50,4 +78,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         )
     )
 
+    logger.info(f"User {user.id} ({user.full_name}) started the conversation.")
     return SET_LANGUAGE
