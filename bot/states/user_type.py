04ce367c3e56a64eedc17c8d84b699ab75dbdb1f
= <=>
2025-01-27 22:31:07 +0200

    refactor: add logger and move bot conversation to the function

--- bot/states/user_type.py
+++ bot/states/user_type.py
@@ -1,48 +1,88 @@
 #!/usr/bin/env python3
-"""This module handles the user's choice of role (Activist or Organization) in a Telegram bot conversation.
+
+"""This module defines the user type selection state for a Telegram bot.
 
 Functions:
     user_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        Handles the user's choice of role and prompts them for the next action.
+        Handles the user's choice of type (activist or organization) and responds accordingly.
 """
 
-
-import logging
-
+from logging import getLogger
 from telegram import (
     Update,
     ReplyKeyboardMarkup
 )
-
 from telegram.ext import ContextTypes
+from ..utils.utilties import define_lang
 from .. import CHOICE, CHOOSE_TASK, UPLOAD_PROFILE
 
-from ..utils.utilties import define_language
-
-logger = logging.getLogger(__name__)
+logger = getLogger(__name__)
 
 
-async def user_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Handles the user's choice of role
-        (Activist or Org) and prompts them for the next action.\
-
-            Args:
-                update (telegram.Update): Incoming update from the user.
-                context (telegram.ext.ContextTypes.DEFAULT_TYPE): Context object containing user data.
-            Returns:
-                int: The next conversation state (CHOOSE_TASK, UPLOAD_PROFILE, or CHOICE).
+async def user_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Handles the user's choice of either 'Activist' or 'Organization' and responds accordingly.
+    Args:
+        update (Update): The update object that contains the user's message.
+        context (ContextTypes.DEFAULT_TYPE): The context object that contains user data and other information.
+    Returns:
+        int: This function returns different states based on the user's choice:
+            - CHOOSE_TASK: If the user selects 'Activist'.
+            - UPLOAD_PROFILE: If the user selects 'Organization'.
+            - CHOICE: If the user enters an invalid choice.
     """
 
-    user_type = update.message.text
-    context.user_data['user_type'] = user_type
+    user_type: str = update.message.text
+    context.user_data['type'] = user_type
+
+    conversation: dict[dict[str]] = {
+        'activist': {
+            'en': ''.join((
+                "<b>Thank you for confirming that you are an activist!</b>\n",
+                "As an activist, I can assist you with the following tasks. Please choose one to proceed:",
+                "\n\n<b>Options:</b>",
+                "\n- Analyze a social problem using the Problem Tree method.",
+                "\n- Create a concept note for your project idea.",
+                "\n- Write a full proposal for your initiative.",
+                "\n\n<b>How to proceed:</b>",
+                " Simply reply with one of the following options: 'Analyze Problem', 'Create Concept Note', or 'Write Full Proposal'."
+            )),
+
+            'ar': ''.join((
+                "<b>شكرًا لتأكيد أنك ناشط!</b>\n",
+                "بصفتك ناشطًا، يمكنني مساعدتك في المهام التالية. يرجى اختيار أحد الخيارات للمتابعة:",
+                "\n\n<b>الخيارات:</b>",
+                "\n- تحليل مشكلة اجتماعية باستخدام طريقة شجرة المشكلة.",
+                "\n- إنشاء مذكرة مفهوم لفكرة مشروعك.",
+                "\n- كتابة مقترح كامل لمبادرتك.",
+                "\n\n<b>كيفية المتابعة:</b>",
+                " ببساطة، قم بالرد بأحد الخيارات التالية: 'Analyze Problem' أو 'Create Concept Note' أو 'Write Full Proposal'."
+            )),
+        },
+        'organization': {
+            'en': ''.join((
+                "<b>Thank you for confirming that you represent an organization!</b>\n",
+                "To get started, please upload your organization's profile as a PDF, DOCX, or DOC file. ",
+            )),
+            'ar': ''.join((
+                "<b>شكرًا لتأكيد أنك تمثل منظمة!</b>\n",
+                "للبدء، يرجى تحميل ملف تعريف المنظمة كملف PDF أو DOCX أو DOC. ",
+            )),
+        },
+        'error': {
+            'en': "Sorry, I didn't understand that. Please choose either 'Activist' or 'Organization'.",
+            'ar': "عذرًا، لم أفهم ذلك. يرجى اختيار 'ناشط' أو 'منظمة'."
+        }
+    }
+
+    text: str = ''
 
     if user_type == "Activist":
-        html_text: str = define_language(
-            'activist', context.user_data['language_code']
+        text = define_lang(
+            conversation['activist'], context.user_data['language_code']
         )
 
         await update.message.reply_text(
-            html_text,
+            text,
             parse_mode='HTML',
             reply_markup=ReplyKeyboardMarkup(
                 [[
@@ -54,25 +94,31 @@ async def user_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
                 resize_keyboard=True
             )
         )
+
         logger.info(f"User selected {user_type}. Proceeding to CHOOSE_TASK.")
 
         return CHOOSE_TASK
     elif user_type == "Organization":
-        html_text: str = define_language(
-            'organization', context.user_data['language_code']
+        text = define_lang(
+            conversation['organization'], context.user_data['language_code']
         )
         await update.message.reply_text(
-            html_text,
+            text,
             parse_mode='HTML'
         )
+
         logger.info(
             f"User selected {user_type}. Proceeding to UPLOAD_PROFILE.")
+
         return UPLOAD_PROFILE
     else:
-        html_text: str = define_language(
-            'error_user_type', context.user_data['language_code'])
+        text = define_lang(
+            conversation['error'], context.user_data['language_code'])
+
         await update.message.reply_text(
-            html_text,
+            text,
             parse_mode='HTML')
-        logger.warning(f"Invalid choice entered by user: '{user_type}'")
+
+        logger.info(f"Invalid choice entered by user: '{user_type}'")
+
         return CHOICE
