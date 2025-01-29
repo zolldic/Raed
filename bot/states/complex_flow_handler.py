0306328ab09efef6547505a27a7d211b8f63bd16
= <=>
2025-01-29 11:02:51 +0200

    feat: add a function to handle complex user operation

---
+++ bot/states/complex_flow_handler.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+This module handles the complex flow of user interactions in a Telegram bot.
+
+It defines the `flow_handler` function which processes user messages and directs
+the flow of conversation based on the user's input. The function uses the 
+`telegram` library to interact with the Telegram API and the `logging` library 
+to log user actions.
+
+Functions:
+    flow_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        Handles user messages and directs the flow of conversation based on the 
+        user's input. Returns the next state of the conversation.
+
+Constants:
+    FLOW_HANDLER: int
+        The state representing the flow handler.
+    CONCEPT_NOTE: int
+        The state representing the generation of a concept note.
+    FULL_PROPOSAL: int
+        The state representing the generation of a full proposal.
+"""
+
+from telegram import Update
+from telegram.ext import ContextTypes, ConversationHandler
+from logging import getLogger
+
+from .. import FLOW_HANDLER, CONCEPT_NOTE, FULL_PROPOSAL
+
+logger = getLogger(__name__)
+
+
+async def flow_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Handles the flow of conversation based on the user's message text.
+    Args:
+        update (Update): The update object that contains the user's message.
+        context (ContextTypes.DEFAULT_TYPE): The context object for the conversation.
+        Returns:
+        int: The next state of the conversation based on the user's selection.
+            - CONCEPT_NOTE: If the user selects 'Generate Concept Note'.
+            - FULL_PROPOSAL: If the user selects 'Generate Full Proposal'.
+            - ConversationHandler.END: If the user selects 'End Conversation'.
+            - FLOW_HANDLER: If the user selects an invalid option.
+    """
+    match update.message.text:
+        case 'Generate Concept Note':
+            logger.info("User selected 'Generate Concept Note'")
+            return CONCEPT_NOTE
+        case 'Generate Full Proposal':
+            logger.info("User selected 'Generate Full Proposal'")
+            return FULL_PROPOSAL
+        case 'End Conversation':
+            logger.info("User selected 'End Conversation'")
+            return ConversationHandler.END
+        case _:
+            await update.message.reply_text(
+                "Please select a valid option",
+                parse_mode='HTML'
+            )
+            return FLOW_HANDLER
